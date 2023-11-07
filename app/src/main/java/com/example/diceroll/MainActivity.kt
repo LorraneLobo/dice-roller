de8eeae56cd27b3a34624a503d86c75b277dceee
Lorrane Lobo <lorrane.dev@gmail.com>
2023-11-07 17:23:15 -0300

    first version roll dice app

--- app/src/main/java/com/example/diceroll/MainActivity.kt
+++ app/src/main/java/com/example/diceroll/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.diceroll
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.diceroll.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        rollDice()
+    }
+
+    private fun rollDice() {
+        binding.btnRollDice.setOnClickListener {
+            val value = (1..6).random()
+            changeDiceImage(value)
+        }
+    }
+
+    private fun changeDiceImage(value: Int) {
+        when (value) {
+            1 -> binding.imgDice.setImageResource(R.drawable.dice1)
+            2 -> binding.imgDice.setImageResource(R.drawable.dice2)
+            3 -> binding.imgDice.setImageResource(R.drawable.dice3)
+            4 -> binding.imgDice.setImageResource(R.drawable.dice4)
+            5 -> binding.imgDice.setImageResource(R.drawable.dice5)
+            6 -> binding.imgDice.setImageResource(R.drawable.dice6)
+            else -> binding.imgDice.setImageResource(R.drawable.dice1)
+        }
+
     }
 }
