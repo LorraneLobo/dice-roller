88c7fd5ccd4bad1f3575607fa56b72cd9d8908f1
Lorrane Lobo <lorrane.dev@gmail.com>
2023-11-13 20:53:54 -0300

    add D20 dice; add expandable library; code refactor;

--- app/src/main/java/com/example/diceroll/MainActivity.kt
+++ app/src/main/java/com/example/diceroll/MainActivity.kt
@@ -1,28 +1,46 @@
 package com.example.diceroll
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.diceroll.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var actualDice = 6
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        rollDice()
+        setClickListeners()
     }
 
-    private fun rollDice() {
+    private fun setClickListeners() {
+        binding.addD20Dice.setOnClickListener {
+            actualDice = 20
+            binding.imgDice.setImageResource(R.drawable.d20)
+        }
+        binding.addD6ClassicDice.setOnClickListener {
+            actualDice = 6
+            rollClassicD6Dice()
+        }
         binding.btnRollDice.setOnClickListener {
-            val value = (1..6).random()
-            changeDiceImage(value)
+            val maxNumber = actualDice
+            if (maxNumber == 6 ) {
+                rollClassicD6Dice()
+            } else {
+                val value = (1..maxNumber).random()
+                binding.tvDiceNumber.visibility = View.VISIBLE
+                binding.tvDiceNumber.text = "$value"
+            }
         }
     }
 
-    private fun changeDiceImage(value: Int) {
+    private fun rollClassicD6Dice() {
+        val value = (1..6).random()
+        binding.tvDiceNumber.visibility = View.GONE
         when (value) {
             1 -> binding.imgDice.setImageResource(R.drawable.dice1)
             2 -> binding.imgDice.setImageResource(R.drawable.dice2)
@@ -32,6 +50,5 @@ class MainActivity : AppCompatActivity() {
             6 -> binding.imgDice.setImageResource(R.drawable.dice6)
             else -> binding.imgDice.setImageResource(R.drawable.dice1)
         }
-
     }
 }
